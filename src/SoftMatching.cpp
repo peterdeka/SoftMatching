7ea2913ffb202fa187648f4b201fb52a457e6aca
Pietro De Caro <pietro.decaro@gmail.com>
2013-09-15 12:54:38 +0200

    Inserita classe CTree e ristrutturato il codice. Introdotto supporto tightness

--- src/SoftMatching.cpp
+++ src/SoftMatching.cpp
@@ -16,21 +16,17 @@ using namespace std;
 #define NUMVARS 10	//numero variabili (quindi nodi dell'albero)
 #define DOMAINS_SIZE 2	//dimensione dei domini delle variabili
 char varDomains[NUMVARS][DOMAINS_SIZE];	//ogni varId è associato al suo dominio (che è un array di valori)
-
 #define DOT_TABLE_BEG "<<FONT POINT-SIZE=\"10\"><TABLE CELLBORDER=\"0\" CELLPADDING=\"1\" CELLSPACING=\"0\">"
+#define DOT_TABLE_END "</TABLE></FONT>>];\n"
+#define MALE_TIGHTNESS 0.8	//percentuale di binary constraint NON nulli
 
-//definisce una tavola di binary constraints che lega valori di preferenza a
-//valori della variabile
-/*class CBinConstrTable{
-	char *value;	//array di valori possibili del dominio
-	float *pref;	//array di preferenze associate
-};
-*/
-
+class CTree;
 //definisce un nodo dell'albero(una variabile quindi)
 class CTreeNode{
 public:
+	CTree *tree;
 	int varId;	//nome della variabile
+	char* domain;
 	float *unaryConstraints;	//constraint unari
 	CTreeNode *father;	//pointer al nodo padre
 	CTreeNode **children;	//pointer ai nodi figli(array)
@@ -42,14 +38,16 @@ public:
 
     	//void addChild (CTreeNode,CBinConstrTable);
     	void genUnaryConstraints();
-    	void genBinaryConstraints();
+    	int genBinaryConstraints();
     	void genChildren(int *curVarId);
     	void JSONSubtree(string *res );
     	void DOTSubtree(string *res,char* rootcall );
 
-    CTreeNode() //destructor
+    CTreeNode(int avarId,char *vardomain) //destructor
     {
-    	varId=-1;
+    	tree=NULL;
+    	varId=avarId;
+    	domain=vardomain;
     	unaryConstraints=NULL;
     	father=NULL;
     	children=NULL;
@@ -68,7 +66,73 @@ public:
 };
 
 
+class CTree{
+public:
+	CTreeNode *root;
+	CTreeNode **linearizedTree;
+	int n_nodes;
+	int n_bin_constraints;
+	float tightness;
 
+	void addNode(CTreeNode *node){
+		linearizedTree[n_nodes++]=node;
+	}
+
+	void setRoot(CTreeNode *node){
+		this->addNode(node);
+		this->root=node;
+	}
+
+	CTree(){
+		root=NULL;
+		tightness=1;
+		n_bin_constraints=0;
+		this->linearizedTree=NULL;
+		this->n_nodes=NULL;
+	};
+
+	CTree(int nodes_num){
+		root=NULL;
+		tightness=1;
+		n_bin_constraints=0;
+		this->linearizedTree=(CTreeNode**)malloc(nodes_num*sizeof(CTreeNode*));
+		this->n_nodes=0;
+	}
+
+	~CTree() //destructor
+	{
+	    	free(this->linearizedTree);
+	};
+
+	void genChildren( CTreeNode *curNode,int child_limit);
+
+};
+
+//genera i figli di un nodo (breadth first) l'ordinamento originale delle variabili
+void CTree::genChildren( CTreeNode *curNode,int child_limit){
+	int childrenleft=child_limit;
+	int child_n=0;
+	if(childrenleft==1)
+		child_n=(int)((float)rand()/(float)RAND_MAX);
+	else
+		child_n=rand() % childrenleft;	//quanti figli? TODO modulo 1 e modulo 0
+
+	curNode->child_n=child_n;
+	curNode->children=(CTreeNode**)malloc(child_n*sizeof(CTreeNode*));
+
+	for(int i=0;i<child_n;i++){
+		CTreeNode* childnode=new CTreeNode(this->n_nodes,varDomains[this->n_nodes]);
+		curNode->children[i]=childnode;
+		childnode->father=curNode;
+		childnode->genUnaryConstraints();
+		this->addNode(childnode);
+	}
+
+	this->n_bin_constraints+=curNode->genBinaryConstraints();
+
+}
+
+//METODI CLASSE TREENODE
 //genera vincoli unari dato il dominio della variabile
 void CTreeNode::genUnaryConstraints(){
 	this->unaryConstraints=(float*)malloc(DOMAINS_SIZE*sizeof(float));
@@ -81,9 +145,9 @@ void CTreeNode::genUnaryConstraints(){
 }
 
 //genera vincoli binari sui figli
-void CTreeNode::genBinaryConstraints(){
+int CTreeNode::genBinaryConstraints(){
 	this->childConstraints=(float***)malloc(this->child_n*sizeof(float**));
-
+	int n_gen_constraints=0;
 	for(int u=0;u<this->child_n;u++){	//per ogni figlio
 		float** tp=(float**)malloc(DOMAINS_SIZE*DOMAINS_SIZE*sizeof(float*));
 		for(int i=0;i<DOMAINS_SIZE;i++){	//assegno valori di preferenza random come prodotto cartesiano
@@ -93,34 +157,14 @@ void CTreeNode::genBinaryConstraints(){
 				if(r==0.0f)
 					r+=0.1f;
 				tp[i][z]=r;
+				n_gen_constraints++;
 			}
 		}
 		this->childConstraints[u]=tp;
 	}
+	return n_gen_constraints;
 }
 
-//genera i figli di un nodo, complicata perchè cosi mantiene (breadth first) l'ordinamento originale delle variabili
-void CTreeNode::genChildren( int *curVarId){
-	int childrenleft=NUMVARS-(*curVarId+1);
-	int child_n=0;
-	if(childrenleft==1)
-		child_n=(int)((float)rand()/(float)RAND_MAX);
-	else
-		child_n=rand() % childrenleft;	//quanti figli? TODO modulo 1 e modulo 0
-
-	this->child_n=child_n;
-	this->children=(CTreeNode**)malloc(child_n*sizeof(CTreeNode*));
-
-	for(int i=0;i<child_n;i++){
-		CTreeNode* childnode=new CTreeNode();
-		this->children[i]=childnode;
-		childnode->varId=++*curVarId;
-		childnode->father=this;
-		childnode->genUnaryConstraints();
-	}
-
-	this->genBinaryConstraints();
-}
 
 void CTreeNode::JSONSubtree(string *res){
 	char svarid[10];
@@ -158,11 +202,11 @@ void CTreeNode::DOTSubtree(string *res,char *rootcall){
 		strcat(svarid,tmp);
 		for(int d=0;d<DOMAINS_SIZE;d++){
 			for(int e=0;e<DOMAINS_SIZE;e++){
-				sprintf(tmp,"<TR><TD>%c%c</TD><TD>%.1f</TD></TR>",varDomains[this->varId][d],varDomains[this->children[i]->varId][e],this->childConstraints[i][d][e]);
+				sprintf(tmp,"<TR><TD>%c%c</TD><TD>%.1f</TD></TR>",this->domain[d],this->children[i]->domain[e],this->childConstraints[i][d][e]);
 				strcat(svarid,tmp);
 			}
 		}
-		strcat(svarid,"</TABLE></FONT>>];\n");
+		strcat(svarid,DOT_TABLE_END);
 		///FINE tabella binary constraints
 		string ssv(svarid);
 		*res+=ssv;
@@ -174,6 +218,11 @@ void CTreeNode::DOTSubtree(string *res,char *rootcall){
 }
 
 //----FINE CLASSE TREENODE
+
+
+
+
+
 
 //funzione che costruisce i domini delle variabili a caso
 void buildVarDomains(){
@@ -188,25 +237,26 @@ void buildVarDomains(){
 }
 
 
-CTreeNode *root;
 
-void buildTree(){
+//generazione albero
+void buildTree(CTree *tree){
+
 	CTreeNode **curarr=(CTreeNode**)malloc(NUMVARS*sizeof(CTreeNode*));
 	CTreeNode **otherarr=(CTreeNode**)malloc(NUMVARS*sizeof(CTreeNode*));
-
-	root=new CTreeNode();
-	root->varId=0;
+	//int curVarId=0;	//tiene traccia identificativo variabile per generazione ordinata USO n_nodes dell'albero
+	CTreeNode *root=new CTreeNode(0,varDomains[0]);
 	root->genUnaryConstraints();
 	//ora aggiungo tutte le altre variabili (nodi)
 	CTreeNode *node=root;
-	int curVarId=0;	//tiene traccia identificativo variabile per generazione ordinata
+	tree->setRoot(root);
 	curarr[0]=root;
 	int curlen=1;
 	int otherlen=0;
-	while(curVarId<NUMVARS-2){	//randomizzo discendenza
+	while(tree->n_nodes<NUMVARS-2){	//randomizzo discendenza
 		for(int i=0;i<curlen;i++){	//elaboro nodi sospesi
 			node=curarr[i];
-			node->genChildren(&curVarId);
+			int child_limit=NUMVARS-tree->n_nodes;
+			tree->genChildren(node,child_limit);
 			for(int j=0;j<node->child_n;j++)
 				otherarr[otherlen++]=node->children[j];
 		}
@@ -223,16 +273,36 @@ void buildTree(){
 	free(otherarr);
 }
 
+//TODO watchout,potrebbe generare intere table di zeri
+//riceve tree e annulla random dei constraint fino a raggiungere il livello di tightness desiderato
+void adjustTightness(CTree *tree,float tightness){
+	//calcolo numero di binconstr da mettere a zero
+	int n_unbind=(float)tree->n_bin_constraints*(1.0f-tightness);
+	int chosen_var,chosen_child;
+	for(int i=0;i<n_unbind;i++){
+		chosen_var=rand() % tree->n_nodes;
+		CTreeNode *n=tree->linearizedTree[chosen_var];
+		if(n->child_n<1){	//nodo foglia
+			i--;
+			continue;
+		}
+		chosen_child=rand() % n->child_n;
+		n->childConstraints[chosen_child][rand() % DOMAINS_SIZE][rand() % DOMAINS_SIZE]=0;
+	}
+
+}
 
 
 int main() {
 	srand((unsigned)time(0));
 	buildVarDomains();
-	buildTree();
+	CTree albero(NUMVARS);
+	buildTree(&albero);
+	adjustTightness(&albero,0.2);
 	string st;
-	//root->JSONSubtree(&st);
 	char rootcall=1;
-	root->DOTSubtree(&st,&rootcall);
+	albero.root->DOTSubtree(&st,&rootcall);
+
 	std::cout << st << '\n';
 	ofstream myfile;
 	myfile.open ("graph.gv");
